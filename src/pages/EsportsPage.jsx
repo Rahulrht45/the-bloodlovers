5d476902a4b241d84dd5d0355aa327820638d853
Rahul <rrahulsarder0010@gmail.com>
2026-05-05 21:57:48 +0600

    fix: resolve broken Footer import in EsportsPage

--- src/pages/EsportsPage.jsx
+++ src/pages/EsportsPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Footer } from '../components/HomeSections';
+import Footer from '../components/Footer';
 
 const EsportsPage = ({ title, description }) => {
     return (
